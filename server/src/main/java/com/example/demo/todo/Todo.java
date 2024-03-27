441af8b152694100cf52892cd73da2ca0fdcd8ee
Matěj <mckillem@tuta.io>
2024-03-27 09:18:27 +0100

    https://trello.com/c/oeGM6arR/42-todo-p%C5%99idat-k-todo-z%C3%A1znamu-pole-p%C5%99i%C5%99azen%C3%BD-stav

--- server/src/main/java/com/example/demo/todo/Todo.java
+++ server/src/main/java/com/example/demo/todo/Todo.java
@@ -1,9 +1,6 @@
 package com.example.demo.todo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,5 +16,7 @@ public class Todo {
 	private String description;
 	private Long createdBy;
 	private boolean visibility = true;
+	@Enumerated(EnumType.STRING)
 	private State state;
+	private Long projectId = null;
 }
