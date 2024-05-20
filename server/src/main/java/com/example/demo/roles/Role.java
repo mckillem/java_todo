7cf522e4ecaca9f7d1bbe57c6bc1049c4293efa4
Matěj <mckillem@tuta.io>
2024-05-20 16:19:16 +0200

    autentikace s chybkou nejde se přihlásit

---
+++ server/src/main/java/com/example/demo/roles/Role.java
@@ -0,0 +1,23 @@
+package com.example.demo.roles;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private ERole name;
+}
--- server/src/main/java/com/example/demo/user/UserController.java
+++ server/src/main/java/com/example/demo/user/UserController.java
@@ -1,12 +1,11 @@
 package com.example.demo.user;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/users")
 @AllArgsConstructor
