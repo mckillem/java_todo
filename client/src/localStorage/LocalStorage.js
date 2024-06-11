00d19002cc6b671f80c70c336214bf73f6102cc2
Matěj <mckillem@tuta.io>
2024-06-11 10:14:42 +0200

    token dostupný z localstorage odkudkoli

--- client/src/localStorage/LocalStorage.js
+++ client/src/localStorage/LocalStorage.js
@@ -14,28 +14,40 @@ export const setEmail = (email) => {
 	localStorage.setItem("email", email);
 }
 
-export const getName = () => {
-	return localStorage.getItem("firstName") + " " + localStorage.getItem("lastName");
-}
+// export const getName = () => {
+// 	return localStorage.getItem("firstName") + " " + localStorage.getItem("lastName");
+// }
+//
+// export const setFirstName = (firstName) => {
+// 	localStorage.setItem("firstName", firstName);
+// }
+//
+// export const setLastName = (lastName) => {
+// 	localStorage.setItem("lastName",lastName);
+// }
 
-export const setFirstName = (firstName) => {
-	localStorage.setItem("firstName", firstName);
-}
-
-export const setLastName = (lastName) => {
-	localStorage.setItem("lastName",lastName);
-}
-
-export const setUsername = (username) => {
+export const setUsernameLocal = (username) => {
 	localStorage.setItem("username",username);
+}
+
+export const getUsernameLocal = () => {
+	return localStorage.getItem("username");
 }
 
 export const setId = (id) => {
 	localStorage.setItem("id",id);
 }
 
+export const getId = () => {
+	return localStorage.getItem("id");
+}
+
 export const setRoles = (roles) => {
 	localStorage.setItem("roles",roles);
+}
+
+export const getRoles = () => {
+	return localStorage.getItem("roles");
 }
 
 export const deleteLocaleStorage = () => {
