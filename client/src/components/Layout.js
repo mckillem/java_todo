a6de16cb74c572ad06356c73942b295e8f907060
Matěj <mckillem@tuta.io>
2024-06-22 11:47:41 +0200

    úprava vzhledu

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -5,7 +5,7 @@ import {Outlet} from "react-router-dom";
 
 const Layout = () => {
 	return (
-		<main className="App">
+		<main>
 			{/*todo: header jsem upravil, držet se html tagů. Co bude main?*/}
 			<Header/>
 			<ListOfProjects/>
